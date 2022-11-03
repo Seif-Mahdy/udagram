9cb58dd06c3b19f858ed8526d42dbd9c4f2a9915
Seif Alaa <seif@cloudypedia.com>
2022-11-03 20:47:00 +0200

    fix backend api url

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "udagram-api.eba-nxp7rhyb.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udagram-api.eba-nxp7rhyb.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
